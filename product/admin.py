09d1d8dfbae4be5a7d040bb084ad79e6ce7c851b
Ali-Ghorbanzadeh <biotick18311@gmail.com>
2024-09-18 23:27:51 +0330

    admin customize

--- product/admin.py
+++ product/admin.py
@@ -1,2 +1,22 @@
 from django.contrib import admin
+from .models import Product, ProductImage, ProductCategory
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    search_fields = ('name',)
+    list_filter = ('price',)
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('alt', 'product', 'image')
+    search_fields = ('alt',)
 
